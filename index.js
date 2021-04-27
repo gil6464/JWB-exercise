eb4284e1bfac1f4934f16195392a7ed0d39487ba
gil6464 <gil6464@gmail.com>
2021-04-27 22:37:43 +0300

    started to solve

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /* write the code to run app.js here */
+const PORT = process.env.PORT || 4000;
+const app = require("./app");
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port", `${PORT}`);
+});
